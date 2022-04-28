e027b85c22fe11d1be42d9f127662ce14a9a60df
Alexander Shapoval <alexander.shapoval@live.com>
2022-04-28 18:38:37 +0300

    Extracted tests into separate folder

---
+++ src/OpenSearch.Models/Asset.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenSearch.Models;
+
+public class Asset
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Uri FileUrl { get; set; }
+    public int[] Features { get; set; }
+    public Guid[] Users { get; set; }
+    public Guid[] UserGroups { get; set; }
+    public Guid[] BusinessUnits { get; set; }
+}
