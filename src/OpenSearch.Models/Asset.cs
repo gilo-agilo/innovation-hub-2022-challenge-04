97c942096669ac731cecfe15387b95442e221ef9
Alexander Shapoval <alexander.shapoval@live.com>
2022-04-28 18:42:02 +0300

    Added solution folders, fixed warnings

--- src/OpenSearch.Models/Asset.cs
+++ src/OpenSearch.Models/Asset.cs
@@ -10,4 +10,17 @@ public class Asset
     public Guid[] Users { get; set; }
     public Guid[] UserGroups { get; set; }
     public Guid[] BusinessUnits { get; set; }
+    
+    public Asset(Guid id, string title, string description, Uri fileUrl, int[] features, 
+        Guid[] users, Guid[] userGroups, Guid[] businessUnits)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        FileUrl = fileUrl;
+        Features = features;
+        Users = users;
+        UserGroups = userGroups;
+        BusinessUnits = businessUnits;
+    }
 }
